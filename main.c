1dad7d715dc7c7b5c1b52bf5f1198bceeeb25e24
trunghq <trunghqth1809005@fpt.edu.vn>
2018-10-05 20:40:35 +0700

    con trỏ

--- main.c
+++ main.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+//int main() {
+//    int *p = (int *) malloc(4 * sizeof(int));
+//    for (int i = 0; i < 4; ++i) {
+//        printf("Vui long nhap phan tu thu %d\n", i + 1);
+//        scanf("%d", p + i);
+//    }
+//    for (int j = 0; j < 4 ; ++j) {
+//        printf("%d\n", *(p+j));
+//    }
+//    return 0;
+//}
+//int main(){
+//    int arrayNunber[5];
+//    int sizeofarray = sizeof(arrayNunber) / sizeof(int);
+// Cách 1: Làm việc thông qua index của mảng.
+//    for (int i = 0; i < sizeofarray; ++i) {
+//        scanf("%d", &arrayNunber[i]);
+//    }
+//    }
 
 int main() {
-    int size = 5;
-    int *pointer;
-
-    for (int i = 0; i < size ; ++i) {
-        printf("Nhap phan tu thu %d", i+1);
-        scanf("%d", pointer);
-        pointer++;
+    int arrayNumber [5];
+    int sizeofarray = 5;
+// Cách 2: Làm việc thông qua con trỏ và di chuyển con trỏ. p++, p--.
+    int *p = &arrayNumber[0];
+    for (int i = 0; i < sizeofarray; ++i) {
+        printf("Vui long nhap so thu %d: ", i+1);
+        scanf("%d", p);
+        p++;
     }
-    pointer--;
-    for (int j = 0; j < size; ++j) {
-        printf("%d", pointer);
-        pointer++;
+    p--;
+    for (int j = 0; j < sizeofarray; ++j) {
+        printf("%d\n" , *p);
+        p--;
     }
     return 0;
 }
